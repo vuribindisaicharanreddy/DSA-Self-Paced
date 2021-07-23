923d5ef85bd5ec14ecdbc64a174693a37b7ceade
Manish Virgat <Manish.11902542@lpu.in>
2021-07-23 10:47:59 +0530

    Added New Files

---
+++ Tracks/17. Graph/DetectCycleInADirectedGraph.py
@@ -0,0 +1,33 @@
+# Rest of the code is already given as Driver Code
+
+class Solution:
+    
+    def bfs(self,adj, s, vis,  mil):
+        
+        vis[s] = True
+        mil[s] = True
+        for u in adj[s]:
+            if not vis[u]:
+                
+                if self.bfs(adj,u,vis,mil):
+                    return True
+            elif mil[u]:
+                return True
+                
+        mil[s] = False
+            
+            
+        return False
+                
+        
+    def isCyclic(self, V, adj):
+        
+        vis = [False]*V
+        for v in range(V):
+            if not vis[v]:
+                mil = [False]*V
+                
+                if self.bfs(adj,v,vis,mil):
+                    return True
+                    
+        return False
---
+++ Tracks/17. Graph/PrintAdjacencyList.cpp
@@ -0,0 +1,19 @@
+// Rest of the code is already given as Driver Code
+
+class Solution 
+{
+    public:
+    vector<vector<int>>printGraph(int V, vector<int> adj[])
+    {
+        vector<vector<int> > y(V);
+        for(int i=0; i<V; i++)
+       {
+           y[i].push_back(i);       
+           for(int x: adj[i])
+           {
+                y[i].push_back(x);
+           }
+       }
+       return y;
+    }
+};
